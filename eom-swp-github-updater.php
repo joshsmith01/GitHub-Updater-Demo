b740472f8dddb98768621a7daaa597a6b65b94d8
Joshua Smith <joshsmith01@me.com>
2017-05-08 15:40:53 -0700

    Initial Commit

---
+++ eom-swp-github-updater.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: EOM SWP Github Updater
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Create a throw away plugin that can be used to demonstrate the installation and update process to
+Version: 1.0
+Author: joshsmith01
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/

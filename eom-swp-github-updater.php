e8a3964f0df43f17f5f7fdced17f4e243611583b
Joshua Smith <joshsmith01@me.com>
2017-05-16 11:48:53 -0700

    Add stylesheet with obvious background color

--- eom-swp-github-updater.php
+++ eom-swp-github-updater.php
@@ -2,10 +2,44 @@
 
 /*
 Plugin Name: EOM SWP Github Updater
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Plugin URI: https://github.com/joshsmith01/GitHub-Updater-Demo
 Description: Create a throw away plugin that can be used to demonstrate the installation and update process to
-Version: 1.0
-Author: joshsmith01
-Author URI: http://URI_Of_The_Plugin_Author
+Version: 0.1.0
+Author: Josh Smith
+Author URI: http://www.efficiencyofmovement.com
 License: A "Slug" license name e.g. GPL2
+GitHub Plugin URI: https://github.com/joshsmith01/GitHub-Updater-Demo
 */
+
+/**
+ * Add a widget to the dashboard.
+ *
+ * This function is hooked into the 'wp_dashboard_setup' action below.
+ */
+function example_add_dashboard_widgets() {
+
+	wp_add_dashboard_widget(
+		'example_dashboard_widget',         // Widget slug.
+		'Example Dashboard Widget',         // Title.
+		'example_dashboard_widget_function' // Display function.
+	);
+}
+
+add_action( 'wp_dashboard_setup', 'example_add_dashboard_widgets' );
+
+/**
+ * Create the function to output the contents of our Dashboard Widget.
+ */
+function example_dashboard_widget_function() {
+
+	// Display whatever it is you want to show.
+	echo "Hello World, I'm a great Dashboard Widget";
+}
+
+
+// Load the stylesheet from the plugin. -JMS
+function load_custom_wp_admin_style() {
+	wp_enqueue_style( 'eom-swp-github-updater-styles', plugins_url( '/css/eom-swp-github-updater-styles.css', __FILE__ ) );
+}
+
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
